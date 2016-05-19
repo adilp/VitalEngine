53550cbfe267c33041df617e79e8e99bec8d98ee
Adil Patel <Adil@MacBook-Pro.local>
2016-05-18 21:36:07 -0500

    Message list with pic, name, message as an object

--- app/src/main/java/com/adilpatel/vitalengine/CustomAdapterMessages.java
+++ app/src/main/java/com/adilpatel/vitalengine/CustomAdapterMessages.java
@@ -1,35 +1,43 @@
 package com.adilpatel.vitalengine;
 
 import android.content.Context;
+import android.graphics.Typeface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import java.util.ArrayList;
 
 /**
  * Created by Adil on 5/17/16.
  */
 public class CustomAdapterMessages extends BaseAdapter {
 
-    int [] images;    String [] names; String [] messages;
+    //int [] images;    String [] names; String [] messages; boolean[] readUnread;
+    ArrayList<OneFragment.MessageData> arrMessageData;
     Context context;
     private static LayoutInflater inflater = null;
 
-    public CustomAdapterMessages(Context mainActivity, int[] images, String [] names, String [] messages) {
-        this.images = images;        this.names = names;
-        this.messages = messages;
+    public CustomAdapterMessages(Context mainActivity, ArrayList<OneFragment.MessageData> arrMessageData){ //int[] images, String [] names, String [] messages, boolean[] readUnread) {
+        //this.images = images;
+        //this.names = names;
+        //this.messages = messages;
+        //this.readUnread = readUnread;
+
+        this.arrMessageData = arrMessageData;
 
         context = mainActivity;
+//
 
         inflater = ( LayoutInflater )context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
-    @Override
+
     public int getCount() {
-        return names.length;
-    }
+        return arrMessageData.size();
+   }
 
     @Override
     public Object getItem(int position) {
@@ -51,22 +59,46 @@ public class CustomAdapterMessages extends BaseAdapter {
         ViewHolder holder=new ViewHolder();
 
         View rootView = inflater.inflate(R.layout.user_list_item , null);
+//        holder.tv = (TextView) rootView.findViewById(R.id.myTV);
+//        holder.iv=(ImageView) rootView.findViewById(R.id.myIV);
+//        holder.tv1= (TextView) rootView.findViewById(R.id.myTV1);
+//        holder.tv.setText(names[position]);
+//        holder.tv1.setText(messages[position]);
+//        holder.iv.setImageResource(images[position]);
+
         holder.tv = (TextView) rootView.findViewById(R.id.myTV);
         holder.iv=(ImageView) rootView.findViewById(R.id.myIV);
         holder.tv1= (TextView) rootView.findViewById(R.id.myTV1);
-        holder.tv.setText(names[position]);
-        holder.tv1.setText(messages[position]);
-        holder.iv.setImageResource(images[position]);
+
+        holder.tv1.setText("" + arrMessageData.get(position).getMessage());
+        holder.tv.setText(""+ arrMessageData.get(position).getName());
+        holder.iv.setImageResource(arrMessageData.get(position).getImage());
+
+        if (arrMessageData.get(position).isRead() == true){
+            holder.tv1.setTypeface(Typeface.DEFAULT_BOLD);
+        }
+
+
+
+//        String message1 = arrMessageData.get(0).getMessage();
+//        String name1 = arrMessageData.get(0).getName();
+//        int pic1 = arrMessageData.get(0).getImage();
+//        holder.tv1.setText(message1);
+//        holder.tv.setText(name1);
+//        holder.iv.setImageResource(pic1);
 
 
 
 
-        rootView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(context, "Clicked item is : " + names[position], Toast.LENGTH_LONG).show();
-            }
-        });
+
+
+
+//        rootView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(context, "Clicked item is : " + names[position], Toast.LENGTH_LONG).show();
+//            }
+//        });
         return rootView;
     }
 }
--- app/src/main/java/com/adilpatel/vitalengine/OneFragment.java
+++ app/src/main/java/com/adilpatel/vitalengine/OneFragment.java
@@ -8,6 +8,8 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 
 public class OneFragment extends Fragment{
 
@@ -17,7 +19,7 @@ public class OneFragment extends Fragment{
     private ListView usersListView;
     String names[] = {"Anant Kharod, MD", "Mustafa Ahmed, MD"};
     String msg[] = {"What time do you want to get started adding more stuff go over the line", "Presentation is tomorrow"};
-    boolean readUnread [] = {true,false};
+    boolean readUnread [] = {false,false};
     public static int [] images={R.drawable.msgone,R.drawable.msgtwo};
 
 
@@ -38,13 +40,72 @@ public class OneFragment extends Fragment{
         View rootView = inflater.inflate(R.layout.fragment_one, container, false);
         usersListView = (ListView) rootView.findViewById(R.id.usersListView);
         //ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, android.R.id.text1, names);
-        CustomAdapterMessages adapter = new CustomAdapterMessages(getActivity().getBaseContext(), images, names, msg);
-        
+
+        ArrayList<MessageData> arrMessageData = new ArrayList<OneFragment.MessageData>();
+
+        MessageData msg1 = new MessageData();
+        msg1.setName("Anant Kharod, MD");
+        msg1.setMessage("What time do you want to get started adding more stuff go over the line");
+        msg1.setImage(R.drawable.msgone);
+
+        MessageData msg2 = new MessageData();
+        msg2.setName("Mustafa Ahmed, MD");
+        msg2.setMessage("Presentation is tomorrow");
+        msg2.setImage(R.drawable.msgtwo);
+        msg2.setRead(true);
+
+        arrMessageData.add(msg1);
+        arrMessageData.add(msg2);
+
+        CustomAdapterMessages adapter = new CustomAdapterMessages(getActivity().getBaseContext(), arrMessageData);
         usersListView.setAdapter(adapter);
 
 
         return rootView;
 
     }
+
+
+
+        public class MessageData {
+
+            String message = "";
+            boolean isRead = false;
+            int images;
+            String name = "";
+
+            public String getMessage() {
+                return message;
+            }
+
+            public void setMessage(String message) {
+                this.message = message;
+            }
+
+            public boolean isRead() {
+                return isRead;
+            }
+
+            public void setRead(boolean isRead) {
+                this.isRead = isRead;
+            }
+
+            public void setImage (int images){
+                this.images = images;
+            }
+
+            public int getImage(){
+                return images;
+            }
+
+            public String getName(){
+                return name;
+            }
+            public void setName(String name){
+                this.name = name;
+            }
+        }
+
+
 
 }
